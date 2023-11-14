e1f6c1265d25bf3f6589af3d183085dc9e75f94f
C <ichengzeyi@gmail.com>
2023-11-14 17:22:16 +0800

    close #32, add env var WITHOUT_CUDA to setup.py, fail if CUDA is not available and WITHOUT_CUDA is not set (#34)
    * close #32, add env var WITHOUT_CUDA to setup.py, throw error if CUDA is not available and WITHOUT_CUDA is not set
    
    * fix previous mistake that didn't build with CUDNN and CUBLAS on CI

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
 import glob
+import platform
 import os
+
 # import shutil
 from os import path
 from setuptools import find_packages, setup
+
 # from typing import List
 import torch
-from torch.utils.cpp_extension import CUDA_HOME, CppExtension, CUDAExtension
+from torch.utils.cpp_extension import CUDA_HOME, CUDNN_HOME, CppExtension, CUDAExtension
 
 torch_ver = [int(x) for x in torch.__version__.split(".")[:2]]
 assert torch_ver >= [1, 8], "Requires PyTorch >= 1.8"
@@ -21,9 +24,10 @@ def fetch_requirements():
 
 def get_version():
     if os.getenv("BUILD_VERSION"):  # In CI
-        version = os.getenv("BUILD_VERSION", "0.0.0")
+        version = os.getenv("BUILD_VERSION")
     else:
-        version_file_path = path.join(path.abspath(path.dirname(__file__)), "version.txt")
+        version_file_path = path.join(path.abspath(path.dirname(__file__)),
+                                      "version.txt")
         version = open(version_file_path, "r").readlines()[0].strip()
 
     # The following is used to build release packages.
@@ -49,17 +53,10 @@ def get_version():
 def get_extensions():
     this_dir = path.dirname(path.abspath(__file__))
     extensions_dir = path.join(this_dir, "sfast", "csrc")
+    include_dirs = [extensions_dir]
 
     sources = glob.glob(path.join(extensions_dir, "**", "*.cpp"),
                         recursive=True)
-
-    from torch.utils.cpp_extension import ROCM_HOME
-
-    is_rocm_pytorch = (True if ((torch.version.hip is not None) and
-                                (ROCM_HOME is not None)) else False)
-    if is_rocm_pytorch:
-        assert torch_ver >= [1, 8], "ROCM support requires PyTorch >= 1.8!"
-
     # common code between cuda and rocm platforms, for hipify version [1,0,0] and later.
     source_cuda = glob.glob(path.join(extensions_dir, "**", "*.cu"),
                             recursive=True)
@@ -69,14 +66,27 @@ def get_extensions():
     extension = CppExtension
 
     extra_compile_args = {"cxx": []}
+    library_dirs = []
+    libraries = []
     define_macros = []
 
-    if (torch.cuda.is_available() and
-        ((CUDA_HOME is not None) or is_rocm_pytorch)) or os.getenv(
-            "FORCE_CUDA", "0") == "1":
+    # if (torch.cuda.is_available()
+    #         and ((CUDA_HOME is not None) or is_rocm_pytorch)):
+    # Skip the above useless check as we will always compile with CUDA support,
+    # and the CI might be running on CPU-only machines.
+    if os.getenv("WITHOUT_CUDA", "0") != "1":
+        assert CUDA_HOME is not None, "Cannot find CUDA installation."
+
         extension = CUDAExtension
         sources += source_cuda
         sources += source_cuda_rt
+
+        from torch.utils.cpp_extension import ROCM_HOME
+
+        is_rocm_pytorch = (True if ((torch.version.hip is not None) and
+                                    (ROCM_HOME is not None)) else False)
+        if is_rocm_pytorch:
+            assert torch_ver >= [1, 8], "ROCM support requires PyTorch >= 1.8!"
 
         if not is_rocm_pytorch:
             define_macros += [("WITH_CUDA", None)]
@@ -102,7 +112,35 @@ def get_extensions():
             if CC is not None:
                 extra_compile_args["nvcc"].append("-ccbin={}".format(CC))
 
-    include_dirs = [extensions_dir]
+        if CUDNN_HOME is None:
+            try:
+                # Try to use the bundled version of CUDNN with PyTorch installation.
+                # This is also used in CI.
+                # CUBLAS is not needed as the downloaded CUDA should include it.
+                from nvidia import cudnn
+            except ImportError:
+                cudnn = None
+
+            if cudnn is not None:
+                print("Using CUDNN from {}".format(cudnn.__file__))
+                cudnn_dir = os.path.dirname(cudnn.__file__)
+                include_dirs.append(os.path.join(cudnn_dir, "include"))
+                # Hope PyTorch knows how to link it correctly.
+                # We only need headers because PyTorch should have
+                # linked the actual library file. (But why not work on Windows?)
+
+                # Make Windows CI happy (unresolved external symbol)
+                # Why Linux does not need this?
+                if platform.system() == "Windows":
+                    library_dirs.append(os.path.join(cudnn_dir, "lib", "x64"))
+
+        # Make Windows CI happy (unresolved external symbol)
+        # Why Linux does not need this?
+        if platform.system() == "Windows":
+            libraries.append("cudnn")
+            libraries.append("cublas")
+    else:
+        print("Compiling without CUDA support")
 
     ext_modules = [
         extension(
@@ -111,6 +149,8 @@ def get_extensions():
             include_dirs=include_dirs,
             define_macros=define_macros,
             extra_compile_args=extra_compile_args,
+            library_dirs=library_dirs,
+            libraries=libraries,
         )
     ]
 
