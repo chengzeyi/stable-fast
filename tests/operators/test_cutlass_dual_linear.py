05008a9e8beb2816ade020b3338cfb5d2ede286a
chengzeyi <ichengzeyi@gmail.com>
2023-12-02 19:07:23 +0800

    add test_benchmark_geglu

--- tests/operators/test_cutlass_dual_linear.py
+++ tests/operators/test_cutlass_dual_linear.py
@@ -32,8 +32,8 @@ class GEGLU(nn.Module):
 
     def forward(self, hidden_states, enable_opt=False):
         if enable_opt:
-            return torch.ops.sfast.cutlass_linear_geglu_unified(hidden_states,
-                self.proj.weight, self.proj.bias)
+            return torch.ops.sfast.cutlass_linear_geglu_unified(
+                hidden_states, self.proj.weight, self.proj.bias)
         else:
             hidden_states, gate = self.proj(hidden_states).chunk(2, dim=-1)
             return hidden_states * self.gelu(gate)
@@ -44,11 +44,37 @@ class GEGLU(nn.Module):
 @pytest.mark.parametrize('in_features', [4, 8, 16])
 @pytest.mark.parametrize('out_features', [4, 8, 16])
 @pytest.mark.parametrize('N', [4, 16])
-def test_cutlass_dual_linear(dtype, in_features, out_features, N):
+def test_geglu(dtype, in_features, out_features, N):
     with torch.no_grad():
         m = GEGLU(in_features, out_features).cuda().to(dtype=dtype).eval()
         x = torch.randn(N, in_features).cuda().to(dtype=dtype)
         out = m(x)
         out_opt = m(x, enable_opt=True)
 
-        torch.testing.assert_close(out_opt, out, rtol=1e-2, atol=1e-2)
+        torch.testing.assert_close(out_opt, out, rtol=2e-2, atol=2e-2)
+
+
+@pytest.mark.parametrize('dtype', [torch.float16, torch.bfloat16])
+@pytest.mark.parametrize('in_features', [256, 512, 1024])
+@pytest.mark.parametrize('out_features', [512, 1024])
+@pytest.mark.parametrize('N', [1, 16, 10000])
+def test_benchmark_geglu(dtype, in_features, out_features, N):
+    with torch.no_grad():
+        m = GEGLU(in_features, out_features).cuda().to(dtype=dtype).eval()
+        x = torch.randn(N, in_features).cuda().to(dtype=dtype)
+        out = m(x)
+        out_opt = m(x, enable_opt=True)
+
+        torch.testing.assert_close(out_opt, out, rtol=2e-2, atol=2e-2)
+
+        start = time.time()
+        for _ in range(1000):
+            out = m(x)
+        end = time.time()
+        print(f"GEGLU: {end - start}")
+
+        start = time.time()
+        for _ in range(1000):
+            out_opt = m(x, enable_opt=True)
+        end = time.time()
+        print(f"GEGLU_OPT: {end - start}")
