e8525c5fca0180bd3844d46742051fdd90a105cb
chengzeyi <ichengzeyi@gmail.com>
2024-01-29 23:57:54 +0800

    improve custom_python_operator error handling

--- src/sfast/utils/custom_python_operator.py
+++ src/sfast/utils/custom_python_operator.py
@@ -1,5 +1,8 @@
+import logging
 import torch
 import sfast
+
+logger = logging.getLogger()
 
 registered_custom_python_operator_names = set()
 
@@ -8,5 +11,14 @@ def register_custom_python_operator(schema, callable):
     name = torch._C.parse_schema(schema).name
     if name in registered_custom_python_operator_names:
         return
-    sfast._C._jit_register_custom_python_operator(schema, callable)
+
+    def wrapper(*args, **kwargs):
+        try:
+            return callable(*args, **kwargs)
+        except Exception:
+            logger.exception(
+                'Exception raised from custom Python operator implementation.')
+            raise
+
+    sfast._C._jit_register_custom_python_operator(schema, wrapper)
     registered_custom_python_operator_names.add(name)
